cc17436acfd6e77b555b5e5a7e300a5a96968509
Ira Lele <ira.lele@cumminscollege.in>
2026-02-03 10:24:30 +0530

    specialCharacter function created

--- src/CharacterValidation.java
+++ src/CharacterValidation.java
@@ -1,4 +1,17 @@
 class CharacterValidation
 {
-    public static boolean 
+    public static boolean hasSpecialCharacter(String password)
+    {
+        boolean hasSpecialCharacter=false;
+
+        for(char ch: password.toCharArray())
+        {
+            if(!Character.isLetterOrDigit(ch))
+            {
+                hasSpecialCharacter=true;
+                break;
+            }
+        }
+        return hasSpecialCharacter;
+    }
 }
