f61e2f06b51b0f9a0f210010df845479263c9ab8
AadnyaKulkarni <aadnya.kulkarni@cumminscollege.in>
2026-02-03 10:48:10 +0530

    upper lower case fns written

--- src/CharacterValidation.java
+++ src/CharacterValidation.java
@@ -1,4 +1,14 @@
+
 class CharacterValidation
 {
-    public static boolean 
+    public static boolean hasUpper(String input){
+
+        boolean flag=false;
+        int i;
+        for(i=0, i<input.length(), )
+
+
+
+        return flag;
+    } 
 }
