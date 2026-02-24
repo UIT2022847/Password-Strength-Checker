6f0b2727a114c20cae9c3eb2df1c63c2ff81a2f7
Ira Lele <ira.lele@cumminscollege.in>
2026-02-24 10:17:19 +0530

    special character validation class added

---
+++ src/SpecialCharacterValidation.java
@@ -0,0 +1,18 @@
+public class SpecialCharacterValidation{
+        public static boolean hasSpecialCharacter(String password)
+    {
+        boolean hasSpecialCharacter=false;
+
+        for(char ch: password.toCharArray())
+        {
+            if(!Character.isLetterOrDigit(ch))
+            {
+                hasSpecialCharacter=true;
+                break;
+            }
+        }
+        return hasSpecialCharacter;
+    }
+
+
+}
